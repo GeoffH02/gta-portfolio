ef66ceff852529ee7331b7e30e46cc1ddf43c3ee
geoffrey <geoffrey02@orange.fr>
2026-09-23 23:35:01 +0200

    [+]Missing /

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 
-  base: "/portfolio",
+  base: "/portfolio/",
 
   define: {
-    CESIUM_BASE_URL: JSON.stringify("/portfoliocesiumStatic/"),
+    CESIUM_BASE_URL: JSON.stringify("/portfolio/cesiumStatic/"),
   },
 });
